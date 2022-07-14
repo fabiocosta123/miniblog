1ca19db3bb6c5c5979bc2d739e2d5b8e75503b4c
Fabio Costa <fabiocostasilvaoficial@gmail.com>
2022-07-13 22:38:48 -0300

    inserindo route, navbar, footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import "./App.css";
 
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// pages
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+
+// components
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <div className="App">
-      <h1>Miniblog</h1>
+      <BrowserRouter>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }

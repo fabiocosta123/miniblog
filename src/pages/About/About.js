6891f0ceb14ed9c560822851ad6b8c75d9a20c8d
Fabio Costa <fabiocostasilvaoficial@gmail.com>
2022-07-23 01:12:10 -0300

    criando bloqueio para paginas que precisam de autenticação

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,10 +1,20 @@
 //CSS
+import { Link } from "react-router-dom";
 import styles from "./About.modules.css";
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className={styles.about}>
+      <h2>
+        Sobre o mini <span>blog</span>
+      </h2>
+      <p>
+        Este projeto consiste em um blog feito com React no front-end e Firebase
+        no back-end
+      </p>
+      <Link to="/posts/create" className="btn">
+        Criar Post
+      </Link>
     </div>
   );
 };

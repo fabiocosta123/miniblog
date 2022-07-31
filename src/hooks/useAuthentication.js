83ef659ea665438804aac5d27b9e1cc3e3b9d2cb
Fabio Costa <fabiocostasilvaoficial@gmail.com>
2022-07-31 14:27:36 -0300

    autenticando login

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -68,6 +68,33 @@ export const useAuthentication = () => {
     signOut(auth);
   };
 
+  // login sing in
+
+  const login = async (data) => {
+    checkIfIsCancelled();
+
+    setLoading(true);
+    setError(false);
+
+    try {
+      await signInWithEmailAndPassword(auth, data.email, data.password);
+      setLoading(false);
+    } catch (error) {
+      let systemErrorMessage;
+
+      if (error.message.includes("user-not-found")) {
+        systemErrorMessage = "Usuario não encontrado";
+      } else if (error.message.includes("wrong-password")) {
+        systemErrorMessage = "Senha incorreta";
+      } else {
+        systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde";
+      }
+
+      setError(systemErrorMessage);
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
     return () => setCancelled(true);
   }, []);
@@ -78,5 +105,6 @@ export const useAuthentication = () => {
     error,
     loading,
     logout,
+    login,
   };
 };

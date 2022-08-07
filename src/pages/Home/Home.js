1da165c6fe3c5a510cc9bbabd8b576292ba0c392
Fabio Costa <fabiocostasilvaoficial@gmail.com>
2022-08-06 23:01:10 -0300

    estruturando a home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,42 @@
+//CSS
 import styles from "./Home.modules.css";
 
+//hooks
+
+import { useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
+
+//components
+
 const Home = () => {
+  const [query, setQuery] = useState("");
+  const [posts] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={styles.home}>
+      <h1>Veja os nossos posts mais recentes</h1>
+      <form onSubmit={handleSubmit} className={styles.search_form}>
+        <input
+          type="text"
+          placeholder="...busque por tags..."
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="btn btn-dark">Pesquisar</button>
+      </form>
+      <div>
+        <h1>Posts...</h1>
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts</p>
+            <Link to="/post/create" className="btn">
+              Criar o primeiro post
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   );
 };

d253548808f1d3e9afe081b92fe169eec951d7c9
Fabio Costa <fabiocostasilvaoficial@gmail.com>
2022-08-14 23:06:44 -0300

    criando pagina de edição

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ import CreatePost from "./pages/CreatePost/CreatePost";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Search from "./pages/Search/Search";
 import Post from "./pages/Post/Post";
+import EditPost from "./pages/EditPost/EditPost";
 
 // components
 import Navbar from "./components/Navbar";
@@ -60,6 +61,10 @@ function App() {
               <Route
                 path="/register"
                 element={!user ? <Register /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/post/edit/:id"
+                element={user ? <EditPost /> : <Navigate to="/login" />}
               />
               <Route
                 path="/post/create"

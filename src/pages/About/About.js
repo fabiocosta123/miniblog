7584be15e951ad40dbf919e711b6a8972bff0ffb
Fabio Costa <fabiocostasilvaoficial@gmail.com>
2022-08-20 20:38:30 -0300

    modificando about

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,10 +1,10 @@
 //CSS
 import { Link } from "react-router-dom";
-import styles from "./About.modules.css";
+import styles from "./About.module.css";
 
 const About = () => {
   return (
-    <div className="about">
+    <div className={styles.about}>
       <h2>
         Sobre o mini <span>blog</span>
       </h2>
@@ -12,7 +12,7 @@ const About = () => {
         Este projeto consiste em um blog feito com React no front-end e Firebase
         no back-end
       </p>
-      <Link to="/posts/create" className="btn">
+      <Link to="/post/create" className="btn">
         Criar Post
       </Link>
     </div>
